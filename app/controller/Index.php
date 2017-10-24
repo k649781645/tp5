58959898cdf019a767f38a78b4bc456bd659b4cb
Jack <649781645@qq.com>
2017-10-24 23:07:37 +0800

    commit in home

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -10,7 +10,47 @@ class Index extends Controller
     {
         $res = new TxlApi();
         $data = $res->getDepartmentsById();
-        echo($data);
-        //return $this->fetch();
+        //echo($data);
+        return $this->fetch();
     }
+
+    public function test()
+    {
+        return 'hello word';
+    }
+
+    public function getDepartmentById($id=13)
+    {
+        $res = new TxlApi();
+        $data = json_decode($res->getDepartmentsById($id));
+        if($data->errcode === 0)
+        {
+            $data = $data->department;
+        }else
+        {
+            $data = array();
+        }
+        //dump(json_encode($data));
+        return trim(json_encode($data),'[]');
+    }
+
+    public function getUserById($id=1,$fetch_child=true)
+    {
+        $res = new TxlApi();
+        $data = json_decode($res->getUserById($id));
+        if($data->errcode === 0)
+        {
+            $data = array(
+                'code' => 0,
+                'msg'  => '',
+                'count'=> 100,
+                'data' => $data->userlist
+            );
+        }else
+        {
+            $data = array();
+        }
+        return $data;
+    }
+
 }
--- app/extend/wx/TxlApi.php
+++ app/extend/wx/TxlApi.php
@@ -25,4 +25,20 @@ class TxlApi
             return '{"errcode":-1,"errmsg":"departmentId is invalid"}';
         }
     }
+
+    /**
+    * 根据部门ID来部门下成员详情
+    * @param  [Number] $id 部门ID (默认为1表示根部门)
+    * @param  [boolean] $fetch_child 默认递归获取子部门下面成员
+    */
+    public function getUserById($id=1,$fetch_child=true)
+    {
+        if($id > 0)
+        {
+            return http_get("https://qyapi.weixin.qq.com/cgi-bin/user/list?access_token=$this->access_token&department_id=$id&fetch_child=$fetch_child");
+        }else
+        {
+            return '{"errcode":-1,"errmsg":"departmentId is invalid"}';
+        }
+    }
 }
